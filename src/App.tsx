75a74c932ff4bf55de19badd85b35170b9a49e95
RAO FAHAD <raofahad046@gmail.com>
2023-01-18 22:36:02 +0500

    Advance Props Types

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import "./App.css";
 import Greet from "./components/Greet";
 import Person from "./components/Person";
 import PersonList from "./components/PersonList";
+import Status from "./components/Status";
+import Heading from "./components/Heading";
+import Oscar from "./components/Oscar";
 
 function App() {
   const personName = {
@@ -28,8 +31,13 @@ function App() {
   return (
     <div className="App">
       <Greet name="Fahad" messageCount={20} isLoggedIn={false} />
-      <Person name={personName} />
-      <PersonList names={nameList} />
+      {/* <Person name={personName} /> */}
+      {/* <PersonList names={nameList} /> */}
+      <Status status="loading" />
+      {/* <Heading>Placeholder text</Heading> */}
+      <Oscar>
+        <Heading>Oscar goes to Leonardo Dicpario!</Heading>
+      </Oscar>
     </div>
   );
 }
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -2,15 +2,16 @@ import { isPropertySignature } from "typescript";
 
 type GreetProps = {
   name: string;
-  messageCount: number;
+  messageCount?: number;
   isLoggedIn: boolean;
 };
 
 const Greet = (props: GreetProps) => {
+  const { messageCount = 0 } = props;
   return (
     <div>
       {props.isLoggedIn
-        ? `Welcome ${props.name}! You have ${props.messageCount} unread messages
+        ? `Welcome ${props.name}! You have ${messageCount} unread messages
             `
         : "Welcome Guest"}
     </div>
