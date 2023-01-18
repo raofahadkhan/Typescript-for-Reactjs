af6b79578d7f2eda6975abadc4be98bf1df51713
RAO FAHAD <raofahad046@gmail.com>
2023-01-18 22:56:40 +0500

    Event Props

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import PersonList from "./components/PersonList";
 import Status from "./components/Status";
 import Heading from "./components/Heading";
 import Oscar from "./components/Oscar";
+import Button from "./components/Button";
+import Input from "./components/Input";
 
 function App() {
   const personName = {
@@ -38,6 +40,13 @@ function App() {
       <Oscar>
         <Heading>Oscar goes to Leonardo Dicpario!</Heading>
       </Oscar>
+      <Button
+        handleClick={(event, id) => {
+          console.log("Button Clicked", event, id);
+        }}
+      />
+      <br />
+      <Input value="" handleChange={(event) => console.log(event)} />
     </div>
   );
 }
