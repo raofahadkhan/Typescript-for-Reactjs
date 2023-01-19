90bb28f9aca712e8e3eb307fcd9a822bd721ed4b
RAO FAHAD <raofahad046@gmail.com>
2023-01-19 23:03:00 +0500

    React Component Props Type

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,8 @@ import Box from "./components/context/Box";
 import { UserContextProvider } from "./components/context/UserContext";
 import User from "./components/context/User";
 import Counter from "./components/class/Counter";
+import Private from "./components/auth/Private";
+import Profile from "./components/auth/Profile";
 
 function App() {
   const personName = {
@@ -58,7 +60,8 @@ function App() {
       {/* <UserContextProvider>
         <User />
       </UserContextProvider> */}
-      <Counter message="The Count Value is" />
+      {/* <Counter message="The Count Value is" /> */}
+      <Private isLoggedIn={true} Component={Profile} />
     </div>
   );
 }
